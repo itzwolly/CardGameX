dddae4a01098439895b972c7b81793dd507e817f
WOLLY-PC\Wolly <wolly.b@hotmail.com>
2018-09-06 18:06:14 +0200

    Initial stage of the networking prototype. Using unity's own updated network manager (UNet). Started working on localhost connecting to a "board".

---
+++ Card_Game_Networking_Prototype/Assets/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager {
+
+	// Use this for initialization
+	private void Start () {
+        
+	}
+}
---
+++ Card_Game_Networking_Prototype/Assets/Scripts/NetworkCameraHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkCameraHandler : NetworkBehaviour {
+    private Camera _camera;
+
+    public override void OnStartLocalPlayer() {
+        _camera = GetComponent<Camera>();
+        _camera.enabled = true;
+    }
+
+    public void OnDisable() {
+        Debug.Log("Camera equals null: " + _camera == null);
+    }
+}
