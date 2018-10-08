e92a5ec2b9f8ec4c1734413484ea34e652e75b20
Wolly <wolly.m.b@gmail.com>
2018-10-09 00:16:57 +0200

    started on authentication and authorization in the game launcher. This only contains the part before actually communicating with the server, working on that right now. After the communication with the server I'll continue with the view modification after you logged in.

---
+++ CardGameLauncher/CardGameLauncher/Scripts/CustomIdentity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace CardGameLauncher.Scripts {
+    public class CustomIdentity : IIdentity {
+        public CustomIdentity(string name, string email, string[] roles) {
+            Name = name;
+            Email = email;
+            Roles = roles;
+        }
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string[] Roles { get; private set; }
+
+        #region IIdentity Members
+        public string AuthenticationType { get { return "Custom authentication"; } }
+
+        public bool IsAuthenticated { get { return !string.IsNullOrEmpty(Name); } }
+        #endregion
+    }
+}
---
+++ CardGameLauncher/CardGameLauncher/Scripts/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameLauncher.Scripts {
+    public interface IAuthenticationService {
+        User AuthenticateUser(string username, string password);
+    }
+}
