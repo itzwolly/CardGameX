24505974fcb1a2abdf734f08ed67bdeb8ff8f75c
ruzamxela <ruzamxela@gmail.com>
2018-12-15 14:50:27 +0100

    added an FMOD plugin, made the scripts, but the project is not tracked yet

---
+++ Card_Game_Photon_Prototype/Assets/Olex/Scripts/DemoSoundTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoSoundTrigger : MonoBehaviour {
+    public int _dummyID;
+	void Start () {
+		
+	}
+	
+	//Demo: pressing Q, W, or E will trigger a sound coming from a respective direction
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q) && _dummyID==1)
+            SoundManager.instance.PlaySound(SoundManager.instance.m_Damage_EventPath, 50.0f, 10.0f, 0f, transform.position);
+        if (Input.GetKeyDown(KeyCode.W) && _dummyID == 2)
+            SoundManager.instance.PlaySound(SoundManager.instance.m_Damage_EventPath, 50.0f, 10.0f, 0f, transform.position);
+        if (Input.GetKeyDown(KeyCode.E) && _dummyID == 3)
+            SoundManager.instance.PlaySound(SoundManager.instance.m_Damage_EventPath, 50.0f, 10.0f, 0f, transform.position);
+    }
+
+    //The generic request for an object to play a sound looks like this:
+    //SoundManager.instance.PlaySound(SoundManager.instance.m_SOUND EFFECT EVENT NAME_EventPath, REVERB (0f-100f), DAMAGE (0f-10f) (obv keep zero if it's not combat related), FIRE INTENSITY (0f-100f), transform.position);
+    //The list of the sound effect event names is found in the Sound Manager.
+    //Music and Fire for now are not functional.
+}
---
+++ Card_Game_Photon_Prototype/Assets/Olex/Scripts/SoundManager.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+    public static SoundManager instance = null;
+    [FMODUnity.EventRef] public string m_ArmorBuff_EventPath;
+    [FMODUnity.EventRef] public string m_ArmorDebuff_EventPath;
+    [FMODUnity.EventRef] public string m_AttackBuff_EventPath;
+    [FMODUnity.EventRef] public string m_AttackDebuff_EventPath;
+    [FMODUnity.EventRef] public string m_BeginTurn_EventPath;
+    [FMODUnity.EventRef] public string m_CardDiscard_EventPath;
+    [FMODUnity.EventRef] public string m_CardDraw_EventPath;
+    [FMODUnity.EventRef] public string m_CardPlay_EventPath;
+    [FMODUnity.EventRef] public string m_CardSelect_EventPath;
+    [FMODUnity.EventRef] public string m_Connected_EventPath;
+    [FMODUnity.EventRef] public string m_Damage_EventPath;
+    [FMODUnity.EventRef] public string m_Disconnected_EventPath;
+    [FMODUnity.EventRef] public string m_EndTurn_EventPath;
+    [FMODUnity.EventRef] public string m_Fire_EventPath;
+    [FMODUnity.EventRef] public string m_Heal_EventPath;
+    [FMODUnity.EventRef] public string m_Music_EventPath;
+    [FMODUnity.EventRef] public string m_Shuffle_EventPath;
+    [FMODUnity.EventRef] public string m_TurboDiscard_EventPath;
+    [FMODUnity.EventRef] public string m_TurboPlay_EventPath;
+    [FMODUnity.EventRef] public string m_TurboSelect_EventPath;
+    [FMODUnity.EventRef] public string m_UIClick_EventPath;
+    [FMODUnity.EventRef] public string m_UIForbidden_EventPath;
+    [FMODUnity.EventRef] public string m_UINotification_EventPath;
+    [FMODUnity.EventRef] public string m_UISelect_EventPath;
+    public bool m_Debug;
+
+
+    // Use this for initialization
+    void Awake () {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SetParameter(FMOD.Studio.EventInstance e, string name, float value)
+    {
+        FMOD.Studio.ParameterInstance parameter;
+        e.getParameter(name, out parameter);
+        if (parameter.Equals(null))
+        {
+            if (m_Debug)
+                Debug.Log("Parameter named: " + name + " does not exist");
+            return;
+        }
+        parameter.setValue(value);
+    }
+
+    public void PlaySound(string m_EventPath, float m_Spaciousness, float m_Damage, float m_FireIntensity, Vector3 m_Position)
+    {
+        if (m_EventPath != null)
+        {
+            FMOD.Studio.EventInstance e = FMODUnity.RuntimeManager.CreateInstance(m_EventPath);
+            e.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(m_Position));
+
+            SetParameter(e, "Spaciousness", m_Spaciousness);
+            SetParameter(e, "Damage", m_Damage);
+            SetParameter(e, "Fire Intensity", m_FireIntensity);
+
+            e.start();
+            e.release(); 
+        }
+    }
+    //public void PlaySoundFire(gameObject SoundSource)
+    //{
+    //    if (m_Fire_EventPath != null)
+    //    {
+    //        FMOD.Studio.EventInstance e = FMODUnity.RuntimeManager.CreateInstance(m_Fire_EventPath);
+    //        e.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(SoundSource.position));
+
+    //        SetParameter(e, "Spaciousness", m_Spaciousness);
+    //        SetParameter(e, "Damage", m_Damage);
+    //        SetParameter(e, "Fire Intensity", m_FireIntensity);
+
+    //        e.start();
+    //        e.release(); //add fire specifics
+    //    }
+    //}
+
+    //public void PlayMusic(gameObject SoundSource)
+    //{
+    //    if (m_Music_EventPath != null)
+    //    {
+    //        FMOD.Studio.EventInstance e = FMODUnity.RuntimeManager.CreateInstance(m_Music_EventPath);
+    //        e.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(SoundSource.position));
+
+    //        SetParameter(e, "Spaciousness", m_Spaciousness);
+    //        SetParameter(e, "Damage", m_Damage);
+    //        SetParameter(e, "Fire Intensity", m_FireIntensity);
+
+    //        e.start();
+    //        e.release(); //add music specifics
+    //    }
+    //} //figure out fire and music later
+
+
+}
